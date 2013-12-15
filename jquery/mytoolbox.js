9a5b272b8fbebf758fe8574cac8709794e24399a
JSHe <JSHe>
2013-12-15 12:39:34 +0800

    test4 v3

--- jquery/mytoolbox.js
+++ jquery/mytoolbox.js
@@ -1,13 +1,14 @@
 (function($) {
     $.fn.mytoolbox = function(settings) {
 		var _defaultSettings = {
+			bind: 'click',
 			callback: function () {
 				alert(this.id);
 			}
 		};
 		var _settings = $.extend(_defaultSettings, settings);
 		return this.each(function() {
-			$(this).click(_settings.callback);
+			$(this).bind(_settings.bind, _settings.callback);
 		});
 	};
 })(jQuery);
