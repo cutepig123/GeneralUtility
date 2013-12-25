f7e27082809e3597a5b1669596b744be59d83fbf
JSHe <JSHe>
2013-12-25 13:36:00 +0800

    init- fail

---
+++ jquery/yangguang/test.js
@@ -0,0 +1,17 @@
+[
+	  {
+		"element": "input",
+	    "type": "button",
+	    "value":"this is a button",
+		"style":"position: absolute; left: 0px; top: 100px;"
+	  },
+	  {
+	    "element": "datatime"
+	  },
+	  {
+		"element": "input",
+	    "type": "edit",
+	    "value":"this is a edit",
+		"style":"position: absolute; left: 100px; top: 100px;"
+	  }
+]
