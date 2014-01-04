547852b205a6bcecb4c94f6d94de5a0abc80228f
JSHe <JSHe>
2014-01-05 03:03:05 +0800

    can work on localhost

--- jquery/MyPocket/AddToPocket.js
+++ jquery/MyPocket/AddToPocket.js
@@ -1,15 +1,26 @@
 (function(){ 
-	function AddToPocket(href_, title_) {
-  $.ajax({
-	url: 'http://localhost/git_repos/GitHub/GeneralUtility/jquery/MyPocket/msg.php',
-	data: {href: href_, title: title_},
-	error: function(xhr) {
-	  alert('Ajax request ∞l…˙Âe’`');
-	},
-	success: function(response) {
-	  $('#msg').html("success! return value:<br> " + response);
-	}
-  });
+
+function AddToPocket(href_, title_)
+{
+var xmlhttp;
+if (window.XMLHttpRequest)
+  {// code for IE7+, Firefox, Chrome, Opera, Safari
+  xmlhttp=new XMLHttpRequest();
+  }
+else
+  {// code for IE6, IE5
+  xmlhttp=new ActiveXObject("Microsoft.XMLHTTP");
+  }
+xmlhttp.onreadystatechange=function()
+  {
+  if (xmlhttp.readyState==4 && xmlhttp.status==200)
+    {
+	alert("success! return value:<br> " + xmlhttp.responseText);
+    }
+  }
+ var url ="http://localhost/git_repos/GitHub/GeneralUtility/jquery/MyPocket/msg.php?href=" + encodeURIComponent(href_) + '&title=' + encodeURIComponent(title_);
+xmlhttp.open("GET",url,true);
+xmlhttp.send();
 };
 
 var t=document,n=t.location.href,r=t.title;
