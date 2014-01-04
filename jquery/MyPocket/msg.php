fe2a7a6a293184e54a3c5b330b97addf898cf5b1
JSHe <JSHe>
2014-01-05 03:15:01 +0800

    across domain succees!

--- jquery/MyPocket/msg.php
+++ jquery/MyPocket/msg.php
@@ -1,19 +1,7 @@
-<html xmlns="http://www.w3.org/1999/xhtml">
-<head>
-    <title>Hello World jQuery!</title>
-    <script src="http://ajax.googleapis.com/ajax/libs/jquery/1.8.3/jquery.min.js"></script>
-</head>
-
 <?php
   $href =$_GET['href'];
   $title = $_GET['title'];
-  echo $href . $title;
+  //echo $href . $title;
 ?>
 
-<body>
-<script type="text/javascript" >
-	//var t=document,n=t.location.href;
-	//alert(n);
-	alert('(msg.php): <?php  echo $href . $title; ?>');
-</script>
-</body>
+alert('(msg.php): <?php  echo $href . $title; ?>');
