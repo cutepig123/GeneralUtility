8423d06f0d1f62c6889badb8bfe0f2b56ac1b711
JSHe <JSHe>
2014-06-02 22:02:12 +0800

    基本能用了，虽然有bug，比如1.真正写得文件数比队列里面的少 2. 这是象征性的写出去

--- chrome_plugin/crawler/phpDownload/2.filecont.php
+++ chrome_plugin/crawler/phpDownload/2.filecont.php
@@ -1,13 +1,35 @@
 <?php
-$url = 'http://www.qq.com';
-$homepage = file_get_contents($url);
-echo $homepage;
-write_file($homepage, 'Data.html');
+
+if(!isset($_POST['name']))
+{
+	echo 'name is not set!' ;
+	die();
+}	
+	
+$name =$_POST['name'];
+$url = $name;
+
+//$_POST['content']
+
 
 function write_file($data, $file) {
-	$fp =fopen($file,'wb');
+	$fp =fopen($file,'ab');
 	fwrite($fp,$data);
 	fclose($fp);
 }
+//echo 'start: ';
+
+if (false)
+{
+	$homepage = file_get_contents($url);
+	//echo $homepage;
+	write_file($url . $homepage . "\n\n", 'Data.log');
+}
+else	
+{
+	//echo 'name: '.  $name;
+	write_file($name."\n", 'Data.txt');
+}
+echo '1';
 
 ?>
