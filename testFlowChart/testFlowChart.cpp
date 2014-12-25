6a1bc9cf863408f09f17bd535c262ae3c3b234b0
JSHe <JSHe>
2014-12-26 02:51:07 +0800

    Convert a flow to alg

--- testFlowChart/testFlowChart.cpp
+++ testFlowChart/testFlowChart.cpp
@@ -480,6 +480,44 @@ public:
 	}
 };
 
+
+class AlgthmMyFlow :public AlgthmCommImpl<AlgthmMyFlow>
+{
+public:
+	Flow flow;
+
+	AlgthmMyFlow()
+	{
+		int mid_add, mid_start, mid_end;
+
+		//设置开始结束pin的数量和类型
+		m_vInPins.resize(2);
+		m_vInPins[0].m_type = "double";
+		m_vInPins[1].m_type = "double";
+
+		m_vOutPins.resize(1);
+		m_vOutPins[0].m_type = "double";
+
+		flow.AddModule(ALG_NAME_START, &mid_start, &m_vInPins);
+		flow.AddModule(ALG_NAME_END, &mid_end, &m_vOutPins);
+
+		flow.AddModule("Alg_Add", &mid_add);
+
+		flow.ConnectModule(mid_start, 0, mid_add, 0);
+		flow.ConnectModule(mid_start, 1, mid_add, 1);
+		flow.ConnectModule(mid_add, 0, mid_end, 0);
+	}
+
+	short run(MyAlgEnv *flowx)
+	{
+		flow.run(&flowx->vInPin, &flowx->vOutPin);
+
+		return 0;
+	}
+
+
+};
+
 // User codes
 void test()
 {
@@ -508,6 +546,7 @@ void test()
 		ResetPool();
 	}
 
+	if (0)
 	{
 		Flow flow;
 
@@ -550,6 +589,21 @@ void test()
 		ResetPool();
 	}
 
+	{
+		RegisterAlgthm("MyFlowAlg", std::shared_ptr<AlgthmBase>(new AlgthmMyFlow));
+
+		Flow flow;
+		int mid_add, mid_start, mid_end;
+		flow.AddModule("AlgthmOutDbl", &mid_start);
+		flow.AddModule("MyFlowAlg", &mid_add);
+		flow.AddModule("AlgthmInDbl", &mid_end);
+
+		flow.ConnectModule(mid_start, 0, mid_add, 0);
+		flow.ConnectModule(mid_start, 0, mid_add, 1);
+		flow.ConnectModule(mid_add, 0, mid_end, 0);
+		flow.run();
+
+	}
 }
 
 int _tmain(int argc, _TCHAR* argv[])
