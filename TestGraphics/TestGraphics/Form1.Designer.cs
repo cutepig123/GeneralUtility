a8f7b92abc0bfadda39b429856fff35c0ca8ee7d
JSHe <JSHe>
2015-07-18 16:54:52 +0800

    Enhance

--- TestGraphics/TestGraphics/Form1.Designer.cs
+++ TestGraphics/TestGraphics/Form1.Designer.cs
@@ -29,11 +29,14 @@
         private void InitializeComponent()
         {
             this.button1 = new System.Windows.Forms.Button();
+            this.statusStrip1 = new System.Windows.Forms.StatusStrip();
+            this.toolStripStatusLabel1 = new System.Windows.Forms.ToolStripStatusLabel();
+            this.statusStrip1.SuspendLayout();
             this.SuspendLayout();
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(197, 3);
+            this.button1.Location = new System.Drawing.Point(206, 19);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(75, 23);
             this.button1.TabIndex = 0;
@@ -41,21 +44,47 @@
             this.button1.UseVisualStyleBackColor = true;
             this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
+            // statusStrip1
+            // 
+            this.statusStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripStatusLabel1});
+            this.statusStrip1.Location = new System.Drawing.Point(0, 240);
+            this.statusStrip1.Name = "statusStrip1";
+            this.statusStrip1.Size = new System.Drawing.Size(284, 22);
+            this.statusStrip1.TabIndex = 1;
+            this.statusStrip1.Text = "statusStrip1";
+            // 
+            // toolStripStatusLabel1
+            // 
+            this.toolStripStatusLabel1.Name = "toolStripStatusLabel1";
+            this.toolStripStatusLabel1.Size = new System.Drawing.Size(131, 17);
+            this.toolStripStatusLabel1.Text = "toolStripStatusLabel1";
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(284, 262);
+            this.Controls.Add(this.statusStrip1);
             this.Controls.Add(this.button1);
             this.Name = "Form1";
             this.Text = "Form1";
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyDown);
+            this.MouseDown += new System.Windows.Forms.MouseEventHandler(this.Form1_MouseDown);
+            this.MouseUp += new System.Windows.Forms.MouseEventHandler(this.Form1_MouseUp);
+            this.statusStrip1.ResumeLayout(false);
+            this.statusStrip1.PerformLayout();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
 
         private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.StatusStrip statusStrip1;
+        private System.Windows.Forms.ToolStripStatusLabel toolStripStatusLabel1;
+
     }
 }
 
--- TestGraphics/TestGraphics/Form1.cs
+++ TestGraphics/TestGraphics/Form1.cs
@@ -1,4 +1,13 @@
-﻿//http://book.51cto.com/art/200811/98820.htm
+﻿/*
+ issues
+ * 1. 如果去掉button，则autoscroll不工作
+ * 2. autoscroll的scrollbar大小不对
+ * 3. Refresh有闪屏现象
+ * 4. 如何让statusbar不随着scrollbar移动？
+ */
+
+//http://book.51cto.com/art/200811/98820.htm
+//https://msdn.microsoft.com/en-us/library/k0fsyd4e%28v=vs.110%29.aspx
 
 using System;
 using System.Collections.Generic;
@@ -18,29 +27,89 @@ namespace TestGraphics
         {
             InitializeComponent();
             mScale = 1;
+            this.AutoScroll = true;
+            this.KeyPreview = true;
+            mImage = new Bitmap("1.bmp");
         }
 
+        Image mImage;
         double mScale;
+
+        protected override void OnScroll(ScrollEventArgs se)
+        {
+            base.OnScroll(se);
+            Invalidate();
+        }
+
         protected override void OnPaint(PaintEventArgs e)
         {
             base.OnPaint(e);
 
             Graphics dc = e.Graphics;
             e.Graphics.ScaleTransform((float)mScale, (float)mScale);
-            
+            e.Graphics.TranslateTransform (-this.HorizontalScroll.Value, -this.VerticalScroll.Value);
+            e.Graphics.InterpolationMode = System.Drawing.Drawing2D.InterpolationMode.NearestNeighbor;
+
+            e.Graphics.DrawImage(
+                mImage,
+                new Rectangle(10, 10, mImage.Width, mImage.Height),  // destination rectangle  
+                0,
+                0,           // upper-left corner of source rectangle
+                mImage.Width,       // width of source rectangle
+                mImage.Height,      // height of source rectangle
+                GraphicsUnit.Pixel,
+                null);
+
             Pen bluePen = new Pen(Color.Blue, 3);
             dc.DrawRectangle(bluePen, 0, 0, 50, 50);
             Pen redPen = new Pen(Color.Red, 2);
             dc.DrawEllipse(redPen, 0, 50, 80, 60);
+
+        }
+
+        void Scale(bool isInc)
+        {
+            if(isInc)
+                mScale = mScale * 2;
+            else 
+                mScale = mScale / 2;
+
+            this.SetAutoScrollMargin((int)(80 * mScale), (int)(110 * mScale));
+            Invalidate();
+        }
+        
+        private void Form1_KeyDown(object sender, KeyEventArgs e)
+        {
+            
+            if (e.Control && e.Shift && e.KeyCode == Keys.P)
+            {
+                MessageBox.Show("Hello");
+            }
+
+            else if(e.Control && e.KeyCode==Keys.P)
+            {
+                Scale(true);
+            }
+
+            else if (e.Control && e.KeyCode == Keys.O)
+            {
+                Scale(false);
+            }
         }
 
         private void button1_Click(object sender, EventArgs e)
         {
-            if (mScale > 4)
-                mScale = 1.0 / 4;
-            else
-                mScale = mScale * 2;
-            Invalidate();
+            Scale(mScale<4);
+        }
+
+        private void Form1_MouseUp(object sender, MouseEventArgs e)
+        {
+
+        }
+
+        private void Form1_MouseDown(object sender, MouseEventArgs e)
+        {
+            toolStripStatusLabel1.Text = String.Format("{0} {1}", e.X, e.Y);
         }
     }
 }
