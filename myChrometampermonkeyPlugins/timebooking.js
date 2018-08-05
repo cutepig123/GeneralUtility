fb3ae94cd8ac03d37a788056c9b5d13d9ddb557b
JSHe <JSHe>
2018-08-05 20:41:00 +0800

    Update

--- myChrometampermonkeyPlugins/timebooking.js
+++ myChrometampermonkeyPlugins/timebooking.js
@@ -7,6 +7,8 @@
 // @match        https://www.citibank.com.hk/*
 // @match        https://www.services.online-banking.hsbc.com.hk/*
 // @match        https://e-banking1.hangseng.com/*
+// @match        https://ebsnew.boc.cn/*
+// @match        https://its.bochk.com/cdc.overview.do
 // @grant          GM_xmlhttpRequest
 // ==/UserScript==
 
@@ -85,20 +87,40 @@
 
 
         var currentLocation = window.location;
-        var title, balance;
+        var title, balance, types=[],bank='';
 
         if(currentLocation.host.indexOf('citibank')>=0){
+            bank = 'citibank';
             title = getInfo("//a[@id='cmlink_AccountNameLink']");
             balance = getInfo("//div[@class='cT-valueItem']/span[@class='cT-balanceIndicator1 ']");
+            for(var i=0; i<balance.length; i++){
+                var typeAndBalance = balance[i].trim().split(' ');
+                if (typeAndBalance.length!=2){
+                    aleret('some problem occurs:' + balance[i]);
+                }
+                var _type = '';
+                if(typeAndBalance[0]=='HKD'){
+                    _type = 'HKD';
+                    balance[i] = typeAndBalance[1];
+                }else if(typeAndBalance[0]=='USD'){
+                    _type = 'USD';
+                    balance[i] = typeAndBalance[1];
+                }
+                else aleret('invalid type:' + typeAndBalance[0]);
+                types.push(_type);
+            }
         }
         else if(currentLocation.host.indexOf('hsbc')>=0){
+            bank = 'hsbc';
             title=['khd', 'usd', 'invest', 'hkd292', 'credit'];
-            var balances_xpath=["//span[@id='l5pIclzHeguKJh2mPS3yjCCav19PYeaKZddPwiPQNFA=||HKD||2A833HKD       HKDPVSAV       ||HK']",
-                           "//span[@id='l5pIclzHeguKJh2mPS3yjCCav19PYeaKZddPwiPQNFA=||USD||2F833USD       USDPVSAV       ||HK']",
-                           "//span[@id='l5pIclzHeguKJh2mPS3yjCCav19PYeaKZddPwiPQNFA=||HKD||2R833HKD       HKDPVINV       ||HK']",
-                           "//span[@class='fr']/span[@id='l5pIclzHeguKJh2mPS3yjIlO2mrWj4_add_vuWd8eH61wjw=||HKD||2C292HKD       HKDSSSSA       ||HK']",
-                           "//span[@id='l5pIclzHeguKJh2mPS3yjL97YmPyxdxUXqsED7ZtWP34iNVKyQjyz8sR7tX9V6Lm||HKD||3PA  HKD       HKDCRAVR       ||HK']"];
+            var balances_xpath=["//span[contains(@id,'||HKD||2A833HKD       HKDPVSAV       ||HK')]",
+                           "//span[contains(@id,'||USD||2F833USD       USDPVSAV       ||HK')]",
+                           "//span[contains(@id,'||HKD||2R833HKD       HKDPVINV       ||HK')]",
+                           "//span[contains(@id,'||HKD||2C292HKD       HKDSSSSA       ||HK')]",
+                           "//span[contains(@id,'||HKD||3PA  HKD       HKDCRAVR       ||HK')]"];
+
             balance = [];
+            types=['HKD', 'USD','HKD','HKD','HKD'];
 
             for(var i=0; i<balances_xpath.length; i++){
                 var t=getInfo(balances_xpath[i]);
@@ -109,19 +131,99 @@
             }
         }
 		else if(currentLocation.host.indexOf('hangseng')>=0){
-            title=['hkd1', 'hkd2', 'credit', 'loan'];
-            balances_xpath=["//table[@id='accSumTable']/tbody/tr[@class='lvl2']/td[@class='right']",
-                           "//table[@id='accSumTable']/tbody/tr[@class='lvl2 last']/td[@class='right']",
-                           "//table[@id='accSumTable']/tbody/tr[@class='greenRow']/td[@class='right']",
-                           "//table[@id='loanSumTable']/tbody/tr[2]/td[@class='right']"];
-            balance = [];
+            bank='hangseng';
 
-            for(i=0; i<balances_xpath.length; i++){
-                t=getInfo(balances_xpath[i]);
-                if(t.length!=1){
-                    alert('t.length not good' + t.toString());
+
+            var Money = getInfo("//table[contains(@id,'SumTable')]/tbody/tr/td[last()]");
+            var Currency = getInfo("//table[contains(@id,'SumTable')]/tbody/tr/td[last()-1]");
+            var Name = getInfo("//table[contains(@id,'SumTable')]/tbody/tr/td[last()-3]");
+
+            if(Money.length !=Currency.length || Money.length !=Name.length){
+                alert('length has problem');
+            }
+
+            title=[];
+            balance=[];
+            types=[];
+            for(var i=0; i<Currency.length; i++){
+                if(Currency[i].trim().length!=0){
+                    title.push(Name[i]);
+
+                    Money[i] = Money[i].trim();
+                    var p = Money[i].indexOf('DR');
+                    var thisban='';
+                    if (p>=0){
+                        thisban = '-' + Money[i].substring(0, p);
+                    }else thisban = Money[i];
+                    balance.push(thisban);
+
+                    types.push(Currency[i]);
                 }
-                balance.push(t[0]);
+            }
+        }
+        else if(currentLocation.host.indexOf('boc.cn')>=0){
+            bank='boc.cn';
+
+
+            var Money = getInfo("//tr[contains(@class,'balance')]/td[last()-1]");
+            var Currency = getInfo("//tr[contains(@class,'balance')]/td[last()-3]");
+            var Name = getInfo("//tr[contains(@class,'balance')]/td[last()-3]");
+
+            if(Money.length !=Currency.length || Money.length !=Name.length){
+                alert('length has problem');
+            }
+
+            title=[];
+            balance=[];
+            types=[];
+            for(var i=0; i<Currency.length; i++){
+                if(Currency[i].trim().length!=0){
+                    title.push(Name[i]);
+
+                    Money[i] = Money[i].trim();
+                    var p = Money[i].indexOf('DR');
+                    var thisban='';
+                    thisban = Money[i];
+                    balance.push(thisban);
+
+                    if(Currency[i].indexOf( '人民币')>=0)
+                        types.push('RMB');
+                    else if(Currency[i].indexOf('港币')>=0)
+                        types.push('HKD');
+                    else{
+                        alert('unknown currency: ' +Currency[i] );
+                        types.push(Currency[i]);
+                    }
+                }
+            }
+        }
+         else if(currentLocation.host.indexOf('bochk')>=0){
+            bank='bochk';
+
+
+            var Money = getInfo("//table[@class='full_width']//td[@class='data_table_swap1_txt data_table_lastcell'][1]/a");
+            var Currency = ['HKD'];
+            var Name = getInfo("/html/body/table[@id='mid']/tbody/tr/td[@id='content_cell']/div[@id='content']/table[@class='full_width'][1]/tbody/tr[2]/td[@class='data_table_swap1_txt'][2]/a");
+
+            if(Money.length !=Currency.length || Money.length !=Name.length){
+                alert('length has problem');
+            }
+
+            title=[];
+            balance=[];
+            types=[];
+            for(var i=0; i<Currency.length; i++){
+                if(Currency[i].trim().length!=0){
+                    title.push(Name[i]);
+
+                    Money[i] = Money[i].trim();
+                    var p = Money[i].indexOf('DR');
+                    var thisban='';
+                    thisban = Money[i];
+                    balance.push(thisban);
+
+                    types.push(Currency[i]);
+                }
             }
         }
         else{
@@ -133,25 +235,28 @@
             alert('logic bug!');return;
         }
 
-        var s=''
+        var s=[];
         for(i=0; i<title.length;i++){
+            s.push({'name':title[i], 'type':types[i], 'balance':balance[i]});
             console.log(">> " + title[i] + "\t " + balance[i]);
-            s += (">> " + title[i] + "\t " + balance[i] + " ");
         }
+
+        var s2={'bank':bank, 'balance':s};
+
+        alert(JSON.stringify(s2));
 
         GM_xmlhttpRequest ( {
             method:     "POST",
             url:        'https://maker.ifttt.com/trigger/bankmoney/with/key/feQcXd0QuePnJb23E97bv',
-            data:       JSON.stringify({"value1":s, "value2":"HELLO"}),
+            data:       JSON.stringify({"value1":s2, "value2":"HELLO"}),
             headers:    {
                 "Content-Type": "application/json"
             },
             onload:     function (response) {
-                console.log ("gut response");
-                alert("Success");
+                console.log ("gut response " + response);
+                alert("Success " + response);
             }
         } );
-
     }
     //create function, it expects 2 values.
     function insertAfter(newElement,targetElement) {
@@ -183,6 +288,12 @@
 		 else if(currentLocation.host.indexOf('hangseng')>=0){
             xpath="//div[@class='messageBar shadow']";
         }
+         else if(currentLocation.host.indexOf('boc.cn')>=0){
+            xpath="//p[@class='AccountOverview_breads bread']/span[@class='second']";
+        }
+        else if(currentLocation.host.indexOf('bochk')>=0){
+            xpath="//td[@id='page_title']/h1";
+        }
         else{
             alert('Invalid url: ' + currentLocation);
         }
@@ -199,6 +310,7 @@
         var btn = document.createElement("a");
         btn.innerText='DoMyTask!';
         btn.addEventListener("click",updateAll);
+        btn.setAttribute('color','red');
 
 
         insertAfter(btn, btnSave);
