40efa96331446f1ee301198327f7fce197216419
jshe <jshe>
2018-08-19 14:48:31 +0800

    update

--- myChrometampermonkeyPlugins/testBankMoneyParser.py
+++ myChrometampermonkeyPlugins/testBankMoneyParser.py
@@ -5,6 +5,7 @@ import sys
 
 infile=sys.argv[1]
 filename = '%s.csv'%datetime.datetime.now().strftime("%Y%m%d")
+data={}
 with open(filename, 'w') as csvfile:
 	fieldnames = ['OccurredAt', 'bank', 'name', 'HKD', 'RMB', 'USD']
 	writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
@@ -24,5 +25,8 @@ with open(filename, 'w') as csvfile:
 		print (t["Value 1"]["bank"])
 		t1=t["Value 1"]
 		for t11 in t1['balance']:
-			writer.writerow({'OccurredAt': t["OccurredAt"], 'bank': t1['bank'], 'name':t11['name'], t11['type']:t11['balance']})
-   
+			s = str(t1['bank']) + str(t11['name']) + str(t11['balance'])
+			if not s in data.keys():
+				data[s]=1
+				writer.writerow({'OccurredAt': t["OccurredAt"], 'bank': t1['bank'], 'name':t11['name'], t11['type']:t11['balance']})
+	 
