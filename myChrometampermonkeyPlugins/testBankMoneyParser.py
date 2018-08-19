9e8306a3a94979a9c99fa7873e40556f323c7d7c
jshe <jshe>
2018-08-19 14:17:28 +0800

    Update

--- myChrometampermonkeyPlugins/testBankMoneyParser.py
+++ myChrometampermonkeyPlugins/testBankMoneyParser.py
@@ -1,14 +1,16 @@
 import json
 import csv
 import datetime
+import sys
 
+infile=sys.argv[1]
 filename = '%s.csv'%datetime.datetime.now().strftime("%Y%m%d")
 with open(filename, 'w') as csvfile:
 	fieldnames = ['OccurredAt', 'bank', 'name', 'HKD', 'RMB', 'USD']
 	writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
 	writer.writeheader()
 	 
-	for line in open('bankmoney2','r').readlines():
+	for line in open(infile,'r').readlines():
 		line = line.strip()
 		if line.endswith('---'):
 			line =line[:-3]
